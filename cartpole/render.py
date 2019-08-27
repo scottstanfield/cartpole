4b1cda0dd799daf1ae0f53cca16a47099fa0ed68
Scott Stanfield <scott@stanfield.io>
2019-08-27 15:59:33 -0700

    refactor

--- cartpole/render.py
+++ cartpole/render.py
@@ -4,29 +4,25 @@ Copied from http://incompleteideas.net/sutton/book/code/pole.c
 permalink: https://perma.cc/C9ZM-652R
 """
 
-import math
-import pyglet
-from pyglet.gl import (gl, glBegin, glBlendFunc, glClearColor, glColor4f, 
-                       glEnable, glEnd, glLineWidth, glPopMatrix, glPushMatrix, 
-                       glRotatef, glTranslatef, glVertex2f, glVertex3f, 
-                       GL_BLEND, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+import math, six
+import pyglet, pyglet.gl
+from pyglet.gl import *
 
-import six
-
-RAD2DEG = 57.29577951308232
+RAD2DEG     = 57.29578
+X_THRESHOLD = 2.4
 
 def draw_cartpole(cartpole, screen_width):
     """ Draws a cartpole on a screen, scaled to screen_width
     """
     # Compute size of cart and pole as a function of screen_width
-    world_width = cartpole.x_threshold*2
-    scale = screen_width/world_width
-    carty = 100  # TOP OF CART
-    polewidth = 10.0
-    polelen = scale * 1.0
-    cartwidth = 50.0
-    cartheight = 30.0
-    
+    world_width = X_THRESHOLD * 2
+    scale       = screen_width/world_width
+    carty       = 100  # TOP OF CART
+    polewidth   = 10.0
+    polelen     = scale * 1.0
+    cartwidth   = 50.0
+    cartheight  = 30.0
+
     # Draw track
     glColor4f(0,0,0,1.0)
     glLineWidth(1.0)
@@ -38,7 +34,7 @@ def draw_cartpole(cartpole, screen_width):
     # Draw Cart
     l, r, t, b = -cartwidth/2, cartwidth/2, cartheight/2, -cartheight/2
     cartx = cartpole.x*scale+screen_width/2.0  # MIDDLE OF CART
-    
+
     glColor4f(0.,0.,0.,1.0)
     glPushMatrix()  # Push Translation
     glTranslatef(cartx, carty, 0)
@@ -48,14 +44,14 @@ def draw_cartpole(cartpole, screen_width):
     glVertex3f(r, t, 0)
     glVertex3f(r, b, 0)
     glEnd()
-    
+
     # Draw Pole
     l, r, t, b = (
         -polewidth/2,
         polewidth/2,
         polelen-polewidth/2,
         -polewidth/2)
-    
+
     glColor4f(.8, .6, .4, 1.0)
     glPushMatrix()  # Push Rotation
     glRotatef(RAD2DEG * -cartpole.theta, 0, 0, 1.0)
@@ -66,7 +62,7 @@ def draw_cartpole(cartpole, screen_width):
     glVertex3f(r, b, 0)
     glEnd()
     glPopMatrix()  # Pop Rotation
-    
+
     # Draw Axle
     radius = polewidth/2
     glColor4f(0.5, 0.5, 0.8, 1.0)
@@ -86,42 +82,39 @@ class Viewer(pyglet.window.Window):
     def __init__(self, width=600, height=400, display=None):
         display = self._get_display(display)
         super().__init__(width=width, height=height, display=display)
-        
+
         self.model = None
 
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-    
-    def _get_display(self, spec):
-        """Convert a display specification (such as :0) into an actual Display
-        object.
 
+    def _get_display(self, spec):
+        """Convert a display specification (such as :0) into an actual Display object.
         Pyglet only supports multiple Displays on Linux.
         """
         if spec is None:
             return None
-        
+
         if isinstance(spec, six.string_types):
             return pyglet.canvas.Display(spec)
-        
+
         raise ValueError("Invalid display specification: {}. \
         (Must be a string like :0 or None.)".format(spec))
 
     def update(self):
-        """ Update the game clock and dispatch game events to render the
-            cart.
+        """ Update the game clock and dispatch game events to render the cart.
         """
         pyglet.clock.tick()
         self.switch_to()
         self.dispatch_events()
         self.dispatch_event('on_draw')
         self.flip()
-    
+
     def on_draw(self):
         """ Draw the cartpole model in the window.
         """
         glClearColor(1, 1, 1, 1)
         self.clear()
-        
+
         draw_cartpole(self.model, self.width)
 
