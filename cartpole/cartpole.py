bf67fc554359f5289bf994c071de4b9841b92d7c
Scott Stanfield <scott@stanfield.io>
2019-08-29 12:31:39 -0700

    Provide defaults for reset

--- cartpole/cartpole.py
+++ cartpole/cartpole.py
@@ -11,69 +11,72 @@ from collections import namedtuple
 DEG_PER_RAD = 0.0174533
 RAD_PER_DEG = 57.2958
 
-CartPoleState = namedtuple('CartPoleState', 'x x_dot theta theta_dot')
+State = namedtuple('State', 'x x_dot theta theta_dot')
 
 class CartPole:
     """ Model for the dynamics of an inverted pendulum
     """
     def __init__(self):
-        self.gravity         = 9.8
-        self.masscart        = 1.0
-        self.masspole        = 0.1
-        self.length          = 0.5  # actually half the pole's length
-        self.force_mag       = 10.0
-        self.tau             = 0.02  # seconds between state updates
+        self.gravity   = 9.8
+        self.masscart  = 1.0
+        self.masspole  = 0.1
+        self.length    = 0.5   # actually half the pole's length
+        self.force_mag = 10.0
+        self.tau       = 0.02  # seconds between state updates
 
-        self.reset()
+        self.x         = 0
+        self.x_dot     = 0
+        self.theta     = 0
+        self.theta_dot = 0
+
+    @property
+    def state(self):
+        return State(self.x, self.x_dot, self.theta, self.theta_dot)
 
     def __str__(self):
         str = f"x:{self.state.x: .3f}  "\
               f"ẋ:{self.state.x_dot: .4f}     "\
               f"θ°:{self.state.theta * RAD_PER_DEG: > 8.3f}  "
-
         return str
 
     def __repr__(self):
         return self.state
 
-    def reset(self):
+    def reset(self, x=0, x_dot=0, theta=0, theta_dot=0):
         """ Reset the model of a cartpole system to it's initial conditions
+        "   theta is in radians
         """
-        self.x         = 0
-        self.x_dot     = 0
-
-        # +/- ~10 degrees off the vertical
-        self.theta     = random.uniform(-0.15, 0.15)
-        self.theta_dot = 0
+        self.x         = x
+        self.x_dot     = x_dot
+        self.theta     = theta
+        self.theta_dot = theta_dot
 
     def step(self, action):
         """ Move the state of the cartpole simulation forward one time unit
         """
-        total_mass = self.masspole + self.masscart
-        polemass_length = (self.masspole * self.length)
+        total_mass      = self.masspole + self.masscart
+        pole_masslength = self.masspole * self.length
 
-        force      = self.force_mag if action else -self.force_mag
-        costheta   = math.cos(self.theta)
-        sintheta   = math.sin(self.theta)
-        temp = (
-            (force + polemass_length * self.theta_dot ** 2 * sintheta)
-            / total_mass)
-        thetaacc = (
+        force           = self.force_mag if action else -self.force_mag
+        costheta        = math.cos(self.theta)
+        sintheta        = math.sin(self.theta)
+
+        temp = (force + pole_masslength * self.theta_dot ** 2 * sintheta) / total_mass
+
+        # theta acceleration
+        theta_dotdot = (
             (self.gravity * sintheta - costheta * temp)
             / (self.length *
                (4.0/3.0 - self.masspole * costheta * costheta /
                 total_mass)))
-        xacc = (
-            temp - polemass_length * thetaacc * costheta / total_mass
-        )
+
+        # x acceleration
+        x_dotdot = temp - pole_masslength * theta_dotdot * costheta / total_mass
+
         self.x         += self.tau * self.x_dot
-        self.x_dot     += self.tau * xacc
+        self.x_dot     += self.tau * x_dotdot
         self.theta     += self.tau * self.theta_dot
-        self.theta_dot += self.tau * thetaacc
+        self.theta_dot += self.tau * theta_dotdot
 
         return self.state
-
-    @property
-    def state(self):
-        return CartPoleState(self.x, self.x_dot, self.theta, self.theta_dot)
 
